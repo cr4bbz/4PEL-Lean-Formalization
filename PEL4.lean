bb1aeaeca49b9d2e7564c7013091af3035ecd52e
cr4bbz <voigtjulianleon@gmail.com>
2026-08-21 19:34:13 +0200

    Import meta-ignorance boundary gate

--- PEL4.lean
+++ PEL4.lean
@@ -17,6 +17,7 @@ import PEL4.ModalKnowledgeLaws
 import PEL4.ModalKnowledgeStrictLaws
 import PEL4.ModalKnowledgeTransitive
 import PEL4.ModalKnowledgeNegativeIntrospection
+import PEL4.ModalKnowledgeIgnoranceBoundary
 import PEL4.ProbabilisticEvidence
 import PEL4.ReliableEvidence
 import PEL4.Revision
