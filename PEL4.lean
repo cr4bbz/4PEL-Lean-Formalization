a82ccaa90342448742281252432ea3d768a623d3
cr4bbz <voigtjulianleon@gmail.com>
2026-08-21 19:15:59 +0200

    Import transitive positive introspection gate

--- PEL4.lean
+++ PEL4.lean
@@ -15,6 +15,7 @@ import PEL4.ModalLanguage
 import PEL4.ModalValidity
 import PEL4.ModalKnowledgeLaws
 import PEL4.ModalKnowledgeStrictLaws
+import PEL4.ModalKnowledgeTransitive
 import PEL4.ProbabilisticEvidence
 import PEL4.ReliableEvidence
 import PEL4.Revision
