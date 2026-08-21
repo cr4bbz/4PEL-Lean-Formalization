99201f7a5ea2412398ad5db9bc7ca189f178ab78
cr4bbz <voigtjulianleon@gmail.com>
2026-08-21 16:53:16 +0200

    Import object-language Fitch gate

--- PEL4.lean
+++ PEL4.lean
@@ -32,6 +32,7 @@ import PEL4.Paradoxes.Knower
 import PEL4.Paradoxes.Sorites
 import PEL4.Paradoxes.SurpriseExamination
 import PEL4.Paradoxes.SurpriseBackwardElimination
+import PEL4.Paradoxes.Fitch
 
 import PEL4.Conservation
 
