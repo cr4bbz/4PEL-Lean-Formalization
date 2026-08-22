6d521c488833bd7fa033de56e4b15e5b35d98bf0
cr4bbz <voigtjulianleon@gmail.com>
2026-08-22 15:13:47 +0200

    Import threshold robustness gate

--- PEL4.lean
+++ PEL4.lean
@@ -29,6 +29,7 @@ import PEL4.ModalDynamicsBeliefInstability
 import PEL4.ModalDynamicsBeliefRestoration
 import PEL4.ModalDynamicsPhaseClassification
 import PEL4.ModalDynamicsReachability
+import PEL4.ModalDynamicsRobustness
 import PEL4.ProbabilisticEvidence
 import PEL4.ReliableEvidence
 import PEL4.Revision
