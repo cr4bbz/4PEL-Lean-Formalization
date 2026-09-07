028b7d1a2e67b1dbb11d44047b842197c0d137c8
cr4bbz <voigtjulianleon@gmail.com>
2026-09-07 18:14:10 +0200

    Wire Gate 11 into the PEL4 build

--- PEL4.lean
+++ PEL4.lean
@@ -16,6 +16,7 @@ import PEL4.TopologicalEvidenceFixedPoints
 import PEL4.TopologicalKnowledgeIdempotence
 import PEL4.TopologicalEvidenceValueFibres
 import PEL4.TopologicalEvidencePhaseNerve
+import PEL4.TopologicalEvidenceAlexandrovNerve
 import PEL4.Evidence
 import PEL4.Diagnostics
 import PEL4.EpistemicStatus
