e5874de698527f5a54afef176385d5e2be937258
cr4bbz <voigtjulianleon@gmail.com>
2026-09-09 10:28:47 +0200

    Gate 5: wire classical recovery into root build

--- PEL4.lean
+++ PEL4.lean
@@ -27,6 +27,7 @@ import PEL4.MatroidEvidenceCircuitAxiomAudit
 import PEL4.MatroidEvidenceMinors
 import PEL4.MatroidEvidenceMinorSemantics
 import PEL4.MatroidEvidenceMinorAxiomAudit
+import PEL4.ClassicalRecovery
 import PEL4.Diagnostics
 import PEL4.EpistemicStatus
 import PEL4.StructuralTransport
