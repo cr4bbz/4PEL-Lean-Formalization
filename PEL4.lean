c70297bd84d9b8b9f962d9a03d98da81b850ed73
cr4bbz <voigtjulianleon@gmail.com>
2026-09-09 10:40:52 +0200

    Gate 5: wire structural recovery into root build

--- PEL4.lean
+++ PEL4.lean
@@ -28,6 +28,7 @@ import PEL4.MatroidEvidenceMinors
 import PEL4.MatroidEvidenceMinorSemantics
 import PEL4.MatroidEvidenceMinorAxiomAudit
 import PEL4.ClassicalRecovery
+import PEL4.ClassicalRecoveryStructural
 import PEL4.ClassicalRecoveryAxiomAudit
 import PEL4.Diagnostics
 import PEL4.EpistemicStatus
