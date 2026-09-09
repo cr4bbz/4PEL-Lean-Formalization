4bd8ef8a0d4d35da8cd63a693bd9c13964e5f665
cr4bbz <voigtjulianleon@gmail.com>
2026-09-09 14:47:51 +0200

    Import Gate 7 probability formula recovery

--- PEL4.lean
+++ PEL4.lean
@@ -38,6 +38,8 @@ import PEL4.LockeanThresholdPhaseTransition
 import PEL4.LockeanThresholdPhaseTransitionAxiomAudit
 import PEL4.ThresholdPhaseGeometry
 import PEL4.ThresholdPhaseGeometryAxiomAudit
+import PEL4.ProbabilityIntegrityFormulaRecovery
+import PEL4.ProbabilityIntegrityFormulaRecoveryAxiomAudit
 import PEL4.Diagnostics
 import PEL4.EpistemicStatus
 import PEL4.StructuralTransport
