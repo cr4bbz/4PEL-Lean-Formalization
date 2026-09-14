50d6efd80914ba5c5d02ec9c00473315a8147b01
cr4bbz <voigtjulianleon@gmail.com>
2026-09-14 11:51:34 +0200

    Gate 59: add axiom audit

---
+++ PEL4/CanonicalEpistemicBeliefsAxiomAudit.lean
@@ -0,0 +1,7 @@
+import PEL4.CanonicalEpistemicBeliefs
+
+#print axioms PEL4.BayesianBeliefEquivalent.refl
+#print axioms PEL4.BayesianBeliefEquivalent.symm
+#print axioms PEL4.BayesianBeliefEquivalent.trans
+#print axioms PEL4.bayesianBelief_split_same_state_equivalent
+#print axioms PEL4.gate59_syntax_not_epistemic_identity
