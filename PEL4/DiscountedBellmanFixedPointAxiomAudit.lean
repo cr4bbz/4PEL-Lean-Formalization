507d9ade582ab84d3dad70b5e92deda801fc0dfd
cr4bbz <voigtjulianleon@gmail.com>
2026-09-14 23:16:16 +0200

    Gate 75: add axiom audit

---
+++ PEL4/DiscountedBellmanFixedPointAxiomAudit.lean
@@ -0,0 +1,8 @@
+import PEL4.DiscountedBellmanFixedPoint
+
+#print axioms PEL4.gate75_fixed_point
+#print axioms PEL4.gate75_difference_halves
+#print axioms PEL4.gate75_residual_halves
+#print axioms PEL4.gate75_fixed_point_unique
+#print axioms PEL4.gate75_iteration_residual_halves
+#print axioms PEL4.gate75_discounted_bellman_fixed_point
