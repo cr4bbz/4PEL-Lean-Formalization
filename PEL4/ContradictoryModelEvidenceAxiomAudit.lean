fbe96a937a4af2ea1cc12756daa130aa8d726960
cr4bbz <voigtjulianleon@gmail.com>
2026-09-15 11:07:31 +0200

    Gate 102: add axiom audit

---
+++ PEL4/ContradictoryModelEvidenceAxiomAudit.lean
@@ -0,0 +1,8 @@
+import PEL4.ContradictoryModelEvidence
+
+#print axioms PEL4.gate102_gap_is_N
+#print axioms PEL4.gate102_conflict_is_B
+#print axioms PEL4.gate102_validation_is_T
+#print axioms PEL4.gate102_refutation_is_F
+#print axioms PEL4.gate102_unknownness_is_not_contradiction
+#print axioms PEL4.gate102_meta_contradiction_is_genuine_glut
