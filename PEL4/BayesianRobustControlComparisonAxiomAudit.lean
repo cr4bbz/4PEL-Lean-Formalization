fa3f439b317429a893c96a0f130f849167d4d4cc
cr4bbz <voigtjulianleon@gmail.com>
2026-09-15 11:57:07 +0200

    Gate 94: add axiom audit

---
+++ PEL4/BayesianRobustControlComparisonAxiomAudit.lean
@@ -0,0 +1,8 @@
+import PEL4.BayesianRobustControlComparison
+
+#print axioms PEL4.gate94_optimistic_bayes_fragile
+#print axioms PEL4.gate94_robust_safe
+#print axioms PEL4.gate94_optimistic_disagreement
+#print axioms PEL4.gate94_skeptical_bayes_safe
+#print axioms PEL4.gate94_skeptical_agreement
+#print axioms PEL4.gate94_bayesian_robust_policy_boundary
