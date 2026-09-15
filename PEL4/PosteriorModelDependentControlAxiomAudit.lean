73052888c6c62f706ec9e43078fc9c7aba57f3e9
cr4bbz <voigtjulianleon@gmail.com>
2026-09-15 09:54:53 +0200

    Gate 88: add axiom audit

---
+++ PEL4/PosteriorModelDependentControlAxiomAudit.lean
@@ -0,0 +1,9 @@
+import PEL4.PosteriorModelDependentControl
+
+#print axioms PEL4.gate88_pass_fragile_value
+#print axioms PEL4.gate88_pass_safe_value
+#print axioms PEL4.gate88_pass_selects_fragile
+#print axioms PEL4.gate88_fail_fragile_value
+#print axioms PEL4.gate88_fail_safe_value
+#print axioms PEL4.gate88_fail_selects_safe
+#print axioms PEL4.gate88_posterior_model_dependent_control
