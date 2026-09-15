c2bca2930f028e39e2e391162ae75e5bdd07f30e
cr4bbz <voigtjulianleon@gmail.com>
2026-09-15 11:58:37 +0200

    Gate 97: add axiom audit

---
+++ PEL4/OpenModelClassAxiomAudit.lean
@@ -0,0 +1,6 @@
+import PEL4.OpenModelClass
+
+#print axioms PEL4.gate97_old_models_miss
+#print axioms PEL4.gate97_unknown_model_exact_fit
+#print axioms PEL4.gate97_expansion_repairs_adequacy
+#print axioms PEL4.gate97_open_model_class_witness
