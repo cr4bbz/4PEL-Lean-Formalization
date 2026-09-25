08b4642907fa733151e1a896927e1d0923b46edb
cr4bbz <voigtjulianleon@gmail.com>
2026-09-25 16:22:51 +0200

    Wire Gate J3 empirical contract into root build

--- PEL4.lean
+++ PEL4.lean
@@ -83,6 +83,7 @@ import PEL4.ModalDynamicsIntermediatePhase
 import PEL4.ProbabilisticEvidence
 import PEL4.JevBridge
 import PEL4.JevBridgeFalsification
+import PEL4.JevBridgeEmpiricalContract
 import PEL4.ReliableEvidence
 import PEL4.Revision
 
